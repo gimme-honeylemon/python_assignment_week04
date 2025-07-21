8cfb1a0ebe5c87d834da65cefe0833cee8c8e58b
gimme-honeylemon <67011734@kmitl.ac.th>
2025-07-21 15:26:46 +0700

    file uploading

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def add(x,y):
+    return x + y
+def subtract(x,y):
+    return x - y
+def multiply(x,y):
+    return x * y
+def divide(x,y):
+    return x / y
+
+if __name__ == "__main__":
+    x = 6
+    y = 2
+    print(add(x, y))
+    print(subtract(x, y))
+    print(multiply(x, y))
+    print(divide(x,y))
